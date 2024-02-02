0ea09e5fdeebc79c8408b4f6bcfda0b4e7ae1df2
Svilen Grandzhan <peructep@gmail.com>
2024-02-01 19:28:46 -0700

    20240201 Ver. 1.2

--- src/components/routerMap.jsx
+++ src/components/routerMap.jsx
@@ -3,10 +3,10 @@ import Actors from "./Actors";
 import Movies from "./Movies";
 
 const ROUTER_MAP = {
-  "/": Movies,
-  "/movies": Movies,
-  "/shows": TVShows,
-  "/actors": Actors,
+  "/": <Movies />,
+  "/movies": <Movies />,
+  "/shows": <TVShows />,
+  "/actors": <Actors />,
 };
 
 export default ROUTER_MAP;

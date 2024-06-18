49a6a8e2043b96071e075aba8ca387ffcb96310c
Svilen Grandzhan <peructep@gmail.com>
2024-06-18 14:03:45 -0700

    Working example with 3 pages 20240618

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
 import "./styles.css";
-import Header from "./components/Header";
-import TVShows from "./components/TVShows";
-import Actors from "./components/Actors";
-import Movies from "./components/Movies";
 import { Route, Routes } from "react-router-dom";
+// import Header from "./components/Header";
+// import TVShows from "./components/TVShows";
+// import Actors from "./components/Actors";
+// import Movies from "./components/Movies";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <>
-      <Header />
+    <div className="App">
       <Routes>
-        <Route
-          path="/"
-          element={<Movies />}
-        />
-        <Route
-          path="/movies"
-          element={<Movies />}
-        />
-        <Route
-          path="/shows"
-          element={<TVShows />}
-        />
-        <Route
-          path="/actors"
-          element={<Actors />}
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+    </div>
+    // <>
+    //   <Header />
+    //   <Routes>
+    //     <Route
+    //       path="/"
+    //       element={<Movies />}
+    //     />
+    //     <Route
+    //       path="/movies"
+    //       element={<Movies />}
+    //     />
+    //     <Route
+    //       path="/shows"
+    //       element={<TVShows />}
+    //     />
+    //     <Route
+    //       path="/actors"
+    //       element={<Actors />}
+    //     />
+    //   </Routes>
+    // </>
   );
 }
 
